075a98c483f6cfb7d164f2da4e0425f51a0e93fc
I Eat Babies <christiankohnen50@gmail.com>
2024-12-05 08:32:27 -0600

    screw it. we do it at home
    Because even if I did it on time, I would have struggled and have been late anyway

--- Assets/Junior-Programmer-Starter-Files/Assets/Scripts/MainManager.cs
+++ Assets/Junior-Programmer-Starter-Files/Assets/Scripts/MainManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -20,22 +21,54 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
     }
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
         MainManager.Instance.TeamColor = color;
     }
-    private void Start()
+    private void Start(ColorPicker colorPicker)
     {
         if (MainManager.Instance != null)
         {
             SetColor(MainManager.Instance.TeamColor);
         }
+
+        colorPicker.SelectColor(Instance.TeamColor);
     }
 
     private void SetColor(Color teamColor)
     {
         throw new NotImplementedException();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }
